4cde4cf4f066c31f015c5550b76d3209ee4b7847
Martin-Santos <90261721+Martin-Santos@users.noreply.github.com>
2024-08-15 12:40:02 -0300

    red de docker creada, no se registran los usuarios

--- frontend/src/Components/register/register.jsx
+++ frontend/src/Components/register/register.jsx
@@ -1,36 +1,88 @@
 import './register.css';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import axios from 'axios';
 
 export default function Register() {
+    const [ showPassword, setShowPassword ] = useState(false);
+
+    const changePasswordVisibility = (e) => {
+        e.preventDefault();
+        setShowPassword(!showPassword);
+    };
+    const changeConfirmPasswordVisibility = (b) => {
+        b.preventDefault();
+        setShowPassword(!showPassword);
+    };
+
+    const [formData, setForm] = useState({
+        usuario: "",
+        email: "",
+        password: "",
+        password2: ""
+      });
+    
+      const handleChange = (e) => {
+        setForm({
+          ...formData,
+          [e.target.name]: e.target.value
+        });
+      };
+    
+      const handleRegister = async (e) => {
+        e.preventDefault();
+    
+        if (formData.password !== formData.password2) {
+          alert("Las contraseñas tienen que coincidir");
+        } else {
+          try {
+            await axios.post('http://localhost:3000/registrar', {
+              formData
+            });
+      
+            alert("Usuario registrado con éxito");
+          } catch (error) {
+            alert("Error el usuario ya existe");
+            console.log("Error al registrar el usuario: ", error);
+          }  
+        }
+      };
+    
+      const formStyle = {
+        height: '85%',
+      };
+    
+      const imageStyle = {
+        height: '70%',
+      };
 
     return (
         <section>
             <div class="form-box">
                 <div class="form-value">
-                    <form action="http://localhost:3000/" method='post'>
+                    <form action="http://localhost:3001/register" method=''>
                         <h2 class="login-title">Sign Up</h2>
                         <div class="inputbox">
-                            <input type="name" id="nombre" name="usuario"  required/>
+                            <input type="name" id="nombre" name="usuario" onChange={handleChange} required/>
                             <label for="">Name</label>
                         </div>
                         <div class="inputbox">
-                            <ion-icon name="lock-closed-outline" />
-                            <input type="password"  id="contrasena" name="password"   required/>
+                            <ion-icon name="lock-closed-outline" onClick={changePasswordVisibility} />
+                            <input type={showPassword ? 'text' : 'password'}  id="contrasena" name="password"  onChange={handleChange} required/>
                             <label for="">Password</label>
                         </div>
                         <div class="inputbox">
-                            <ion-icon name="lock-closed-outline" />
-                            <input type="password" id="repetir-contrasena" name="password2"  required/>
+                            <ion-icon name="lock-closed-outline" onClick={changeConfirmPasswordVisibility} />
+                            <input type={showPassword ? 'text' : 'password'} id="repetir-contrasena" name="password2"  onChange={handleChange} required/>
                             <label for="">Repeat Password</label>
                         </div>
                         <div class="inputbox">
                             <ion-icon name="mail-outline"></ion-icon>
-                            <input type="email" id="email" name="email" required/>
+                            <input type="email" id="email" name="email" onChange={handleChange} required/>
                             <label for="">Email</label>
                         </div>
                         <p><Link to='/login' >Volver al Login</Link></p>
-                        <button class="boton-login" type="submit">Regístrate</button>
+                        <button class="boton-login" type="submit" onClick={handleRegister}>Regístrate</button>
                     </form>
                 </div>
             </div>
