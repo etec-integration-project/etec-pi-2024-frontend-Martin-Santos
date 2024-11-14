58182fd1ff6fe41f3c19e30afb31760be1cb5e06
Martin Santos <martin.santos.curadelli@gmail.com>
2024-11-13 21:15:15 -0300

    minor changes have been done #10

--- frontend/src/Components/productos/createProduct.jsx
+++ frontend/src/Components/productos/createProduct.jsx
@@ -1,5 +1,5 @@
 import "./productos.css";
-import {useNavigate} from "react-router-dom";
+import {useNavigate, Link} from "react-router-dom";
 import { useState } from 'react';
 import axios from 'axios';
 
